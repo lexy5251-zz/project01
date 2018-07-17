577361a30f44e48146e839161c29e289effa3fd6
Xi <xixi5251@gmail.com>
2018-07-16 23:12:51 -0400

    Project1.2

--- js/aloha.js
+++ js/aloha.js
@@ -1,25 +1,40 @@
-console.log("hello~~~~~~~~~~~~~~~");
-$(".list-editor").on("click", "button", function() {
-	$(".my-list").append(
-		"<li>" +
-			"<span>" +
-			$("input[name='new-item']").val() +
-			"</span>" +
-			'<a href="#" class="delete">[done]</a><button>remove</button>' +
-			"</li>"
-	);
-});
+$(document).ready(function() {
+	$(".main-cloth").flickity({
+		// options
+		cellAlign: "left",
+		contain: true,
+		autoPlay: true
+	});
 
-$(".my-list").on("click", "button", function(event) {
-	event.preventDefault();
-	$(this)
-		.parent()
-		.remove();
-});
+	// $("#email-form").on("submit", function(event) {
+	// 	event.preventDefault();
 
-$(".my-list").on("click", "a", function(event) {
-	event.preventDefault();
-	$(this)
-		.siblings("span")
-		.css("text-decoration", "line-through");
+	// 	if ($("#email-content").val() == "") {
+	// 		alert("You missed your email.");
+	// 	} else {
+	// 		alert("Thank you for subscribing.");
+	// 	}
+	// });
+
+	function validateEmail(email) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	}
+
+	function validate() {
+		var $result = $("#result");
+		var email = $("#email-content").val();
+		$result.text("");
+
+		if (validateEmail(email)) {
+			$result.text(email + " is valid :)");
+			$result.css("color", "green");
+		} else {
+			$result.text(email + " is not valid :(");
+			$result.css("color", "red");
+		}
+		return false;
+	}
+
+	$("#subscribe").bind("click", validate);
 });
